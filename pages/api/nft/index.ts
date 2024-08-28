0c451275d69377be66be84d6eed569c95ef83946
treesirop <1842678637@qq.com>
2024-08-28 11:08:16 +0800

    add

--- pages/api/nft/index.ts
+++ pages/api/nft/index.ts
@@ -46,6 +46,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         const serializedNfts = NFTs.map(nft => {
           return {
             ...nft,
+            tokenId:nft.tokenId.toString(),
             userId: nft.userId.toString(),
           };
         });
